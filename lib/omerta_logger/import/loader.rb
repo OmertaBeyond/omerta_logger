48248f9603db598007f33fa94c5fceb63c83c9df
MurderInc <MurderInc@faffie.biz>
2014-08-27 17:43:37 +0200

    use <version> in XML feed if available

--- lib/omerta_logger/import/loader.rb
+++ lib/omerta_logger/import/loader.rb
@@ -18,11 +18,20 @@ module OmertaLogger
 
       def import
         domain = Domain.find_by_name!(@domain)
-        @version = domain.versions.current
         @xml = Nokogiri::XML(open(domain.api_url)) do |config|
           config.strict.nonet
         end
         @generated = Time.at(xml.css("generated").first.text.to_i)
+        xml_version = xml.css("version").first
+        if xml_version.nil?
+          @version = domain.versions.current
+        else
+          begin
+            @version = domain.versions.find_by!(version: xml_version.text)
+          rescue ActiveRecord::RecordNotFound
+            @version = domain.versions.create(version: xml_version.text, start: @generated)
+          end
+        end
         family_import = Family.new(self)
         if @families
           family_import.import_families
